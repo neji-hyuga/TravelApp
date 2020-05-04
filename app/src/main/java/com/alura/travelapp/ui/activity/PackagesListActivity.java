31be1c309ff93f0d6c8fd9a914fc385c4e473e1e
unknown <viiktoor@gmail.com>
2020-05-04 12:30:00 -0400

    project has created

---
+++ app/src/main/java/com/alura/travelapp/ui/activity/PackagesListActivity.java
@@ -0,0 +1,16 @@
+package com.alura.travelapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alura.travelapp.R;
+
+public class PackagesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages_list);
+    }
+}
